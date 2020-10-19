aea87784be3a377c16b4841ce06fbab98877a714
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-10-19 06:42:19 -0700

    Update auto.sh

--- main/auto.sh
+++ main/auto.sh
@@ -19,7 +19,7 @@ echo '
 printf "\n"
 printf "                \e[101m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
 printf "\n"
-printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m youtube.com/noobhackers \e[100m\e[1;77m << \e[0m\n"
+printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 echo
 read -p "[#] User Name : " usrnm
